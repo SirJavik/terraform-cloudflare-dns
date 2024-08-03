865fd1a04caf4625e04c6f12f8e45c0d069f560e
Benjamin Schneider <ich@benjamin-schneider.com>
2024-08-03 15:09:42 +0200

    Add required providers

---
+++ providers.tf
@@ -0,0 +1,30 @@
+######################################
+#           _             _ _        #
+#          | |           (_) |       #
+#          | | __ ___   ___| | __    #
+#      _   | |/ _` \ \ / / | |/ /    #
+#     | |__| | (_| |\ V /| |   <     #
+#      \____/ \__,_| \_/ |_|_|\_\    #
+#                                    #
+######################################
+
+# Filename: providers.tf
+# Description: 
+# Version: 1.0.0
+# Author: Benjamin Schneider <ich@benjamin-schneider.com>
+# Date: 2024-08-03
+# Last Modified: 2024-08-03
+# Changelog: 
+# 1.0.0 - Initial version
+
+terraform {
+  backend "http" {
+  }
+
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.38.0"
+    }
+  }
+}
